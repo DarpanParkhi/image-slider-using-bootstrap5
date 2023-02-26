01d074339026c1167f4c517ebca07d560570a62b
DarpanParkhi <darpanparkhi03@gmail.com>
2023-02-26 12:58:44 +0530

    added and modified index.js & index.html

---
+++ index.js
@@ -0,0 +1,11 @@
+function cLick1(){
+    alert("please know more about birds");
+}
+
+function cLick2(){
+    alert("please know more about the animals");
+}
+
+function cLick3(){
+    alert("please know more about nature");
+}
